92d1d54e1ef8f3a9b866f9ac929ba851d38d7f33
soncoiz02 <66171585+soncoiz02@users.noreply.github.com>
2022-05-13 13:53:42 +0700

    create router

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
 import React from "react";
+import { BrowserRouter } from "react-router-dom";
+import Router from "./router/Router";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <BrowserRouter>
+      <Router />
+    </BrowserRouter>
+  );
 }
 
 export default App;
